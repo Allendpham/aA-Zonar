5a229959b2f9b1cfa131dbfbc1949ad24fdc0a5f
Allen Pham <Allendpham@gmail.com>
2022-11-14 13:44:25 -0600

    removed undos in seed all

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -17,8 +17,8 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_server_admins()
-        undo_server_users()
+        # undo_server_admins()
+        # undo_server_users()
         undo_channels()
         undo_servers()
         undo_users()
@@ -36,5 +36,5 @@ def undo():
     # Add other undo functions here
     undo_servers()
     undo_channels()
-    undo_server_users()
-    undo_server_admins()
+    # undo_server_users()
+    # undo_server_admins()
