9ab3220e1157978a622a44e9e6d415bf1b9469f8
Allen Pham <Allendpham@gmail.com>
2022-11-14 13:32:48 -0600

    undo commands reversed in seed all function

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -17,11 +17,11 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_servers()
-        undo_channels()
-        undo_server_users()
         undo_server_admins()
+        undo_server_users()
+        undo_channels()
+        undo_servers()
+        undo_users()
     seed_users()
     # Add other seed functions here
     seed_servers()
