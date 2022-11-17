2f4734da30f70243d60999c062400b9993f72d29
TheBabblingBrin <bhooverlee27@gmail.com>
2022-11-17 16:03:37 -0500

    deleted most of nav bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,26 +8,6 @@ const NavBar = () => {
     <nav>
       <ul>
         <li>
-          <NavLink to='/@me' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
           <LogoutButton />
         </li>
       </ul>
