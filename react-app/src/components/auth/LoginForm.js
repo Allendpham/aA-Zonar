6df2d8e16b2a82456fc1409de38a636f7ab5f5f6
Kyle Solano <kylegsolano@gmail.com>
2022-11-16 20:32:42 -0800

    css for login page

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import "./login.css";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,53 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className="login-page">
+      <form className="login-form" onSubmit={onLogin}>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <h2 className="login-welcome">Welcome Back!</h2>
+        <h3 className="login-subheading">We're so excited to see you again!</h3>
+        <div className="login-text-div">
+          <label htmlFor="email">
+            <p className="login-text">Email</p>
+          </label>
+          
+          <input
+            className="login-input"
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className="login-text-div">
+          <label htmlFor="password">
+            <p className="login-text">Password</p>
+          </label>
+          
+          <input
+            className="login-input"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={updatePassword}
+          />
+          <br />
+          <button className='login-submit' type="submit">Log in</button>
+        </div>
+        <p className='register'>
+          Need an account?{" "}
+          <Link className="signup-link" to={`/sign-up`}>
+            Register
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
 
